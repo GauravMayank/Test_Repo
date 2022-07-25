bf6f3c0077962e6b64d70a8763347289d0ad5516
GauravMayank <107171397+GauravMayank@users.noreply.github.com>
2022-07-26 00:08:36 +0530

    Update Release_note.groovy

--- Release_note.groovy
+++ Release_note.groovy
@@ -45,21 +45,21 @@ cd "${REPO_DIR}"
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Switch to production branch
 if [ $CURRENT_BRANCH != "$BRANCH" ]; then
-    conditional_echo "- Switching from $CURRENT_BRANCH to $BRANCH branch. (stashing any local change)"
+    echo "- Switching from $CURRENT_BRANCH to $BRANCH branch. (stashing any local change)"
     # stash any current work
     git stash "${GITPARAMS[@]}"
     # go to the production branch
     git checkout $BRANCH "${GITPARAMS[@]}"
 fi
 
-conditional_echo "- Updating local $BRANCH branch."
+echo "- Updating local $BRANCH branch."
 # pull latest version of production branch
 git pull $REMOTE $BRANCH "${GITPARAMS[@]}"
 # fetch remote, to get latest tags
 git fetch $REMOTE "${GITPARAMS[@]}"
 
 # Get previous release tags
-conditional_echo "- Getting previous tag."
+echo "- Getting previous tag."
 PREVIOUS_TAG=$(echo $(git ls-remote --tags --ref --sort="v:refname" $REMOTE | tail -n1))
 
 # If specific commit not set, get the from the previous release.
@@ -68,19 +68,19 @@ if [ -z "$PREVIOUS_COMMIT" ]; then
     PREVIOUS_COMMIT=$(echo $PREVIOUS_TAG | cut -d' ' -f 1)
 fi
 
-conditional_echo "-- PREVIOUS TAG: $PREVIOUS_TAG"
+echo "-- PREVIOUS TAG: $PREVIOUS_TAG"
 
 # Get previous release number
 PREVIOUS_RELEASE=$(echo $PREVIOUS_TAG | cut -d'/' -f 3 | cut -d'v' -f2 )
 
-conditional_echo "- Creating release tag"
+echo "- Creating release tag"
 # Get last commit
 LASTCOMMIT=$(echo $(git rev-parse $REMOTE/$BRANCH))
 # Check if commit already has a tag
 NEEDSTAG=$(echo $(git describe --contains $LASTCOMMIT 2>/dev/null))
 
 if [ -z "$NEEDSTAG" ]; then
-    conditional_echo "-- Generating release number ($VERSIONTYPE)"
+    echo "-- Generating release number ($VERSIONTYPE)"
     # Replace . with spaces so that can split into an array.
     VERSION_BITS=(${PREVIOUS_RELEASE//./ })
     # Get number parts, only the digits.
@@ -102,40 +102,40 @@ if [ -z "$NEEDSTAG" ]; then
 
     # Create new tag number
     NEWTAG="v$VNUM1.$VNUM2.$VNUM3"
-    conditional_echo "-- Release number: $NEWTAG"
+    echo "-- Release number: $NEWTAG"
     # Check to see if new tag already exists
     TAGEXISTS=$(echo $(git ls-remote --tags --ref $REMOTE | grep "$NEWTAG"))
 
     if [ -z "$TAGEXISTS" ]; then
         # Check if release notes were not provided.
         if [ -z "$RELEASENOTES" ]; then
-            conditional_echo "- Generating basic release notes of commits since last release."
+            echo "- Generating basic release notes of commits since last release."
             # Generate a list of commit messages since the last release.
             RELEASENOTES=$(git log --pretty=format:"- %s" $PREVIOUS_COMMIT...$LASTCOMMIT  --no-merges)
         fi
         # Tag the commit.
         if [[ "$DRYRUN" -eq 0 ]]; then
-            conditional_echo "-- Tagging commit. ($LASTCOMMIT)"
+            echo "-- Tagging commit. ($LASTCOMMIT)"
             git tag -a $NEWTAG -m"$RELEASEDATE: Release $VNUM1.$VNUM2.$VNUM3" -m"$RELEASENOTES" $LASTCOMMIT
-            conditional_echo "- Pushing release to $REMOTE"
+            echo "- Pushing release to $REMOTE"
             # Push up the tag
             git push $REMOTE $NEWTAG "${GITPARAMS[@]}"
         else
-            conditional_echo "Release Notes:"
-            conditional_echo "$RELEASENOTES"
+            echo "Release Notes:"
+            echo "$RELEASENOTES"
         fi
     else
-        conditional_echo "-- ERROR: TAG $NEWTAG already exists."
+        echo "-- ERROR: TAG $NEWTAG already exists."
         exit 1
     fi
 else
-    conditional_echo "-- ERROR: Commit already tagged as a release. ($LASTCOMMIT)"
+    echo "-- ERROR: Commit already tagged as a release. ($LASTCOMMIT)"
     exit 1
 fi
 
 # Switch to back to original branch
 if [ $CURRENT_BRANCH != "$BRANCH" ]; then
-    conditional_echo "- Switching back to $CURRENT_BRANCH branch. (restoring local changes)"
+    echo "- Switching back to $CURRENT_BRANCH branch. (restoring local changes)"
     git checkout "$CURRENT_BRANCH" "${GITPARAMS[@]}"
     # remove the stash
     git stash pop "${GITPARAMS[@]}"
