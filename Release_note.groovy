85564e531a5d4f9d41b0638c92398551c9eb6377
GauravMayank <107171397+GauravMayank@users.noreply.github.com>
2022-07-26 17:40:09 +0530

    Update Release_note.groovy

--- Release_note.groovy
+++ Release_note.groovy
@@ -151,7 +151,12 @@ exit 0
   }
 }
 node("dev-mini-housing-jenkins-slave") {
-   git_clone()   
-   release_job()
-      }
+   if("${env.BRANCH_NAME}" == "master"){
+   env.newtag="${TAG}"
+   echo "${newtag}"
+   env.imagetag="628119511333.dkr.ecr.ap-southeast-1.amazonaws.com/housing/${repo}:${TAG}"
+   git_clone()
+   build()
+   push_artifact()
+   }
       
